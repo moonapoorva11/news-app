37501bd8764aaec1539fe4acd74c849b2ad5399d
moon123apoorva <moon2018apoorva@gmail.com>
2025-09-04 00:01:55 +0530

    completed project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import Navbar from './Components/Navbar'
+import NewsBoard from './Components/NewsBoard'
+
+const App = () => {
+
+  const[category,setcategory] =useState("general");
+  return (
+    <div>
+      <Navbar setcategory={setcategory}/>
+      <NewsBoard category={category}/>
+     </div>
+  )
+}
+
+export default App
